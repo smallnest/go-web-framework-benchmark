2568141f8f6f9a2028ac170f304e2a20c6a200f6
smallnest <smallnest@gmail.com>
2016-07-01 13:13:39 +0800

    modify libs.sh to split one long line to multiple lines

--- libs.sh
+++ libs.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-web_frameworks=( "default" "ace" "beego" "bone" "denco" "echov1" "echov2standard" "echov2fasthttp" "fasthttp-raw" "fasthttprouter" "fasthttp-routing" "gas" gin" "gocraftWeb" "goji" "gojiv2" "gojsonrest" "gorestful" "gorilla" "httprouter" "httptreemux" "iris" "lars" "lion" "macaron" "martini" "pat" "possum" "r2router" "tango" "tiger" "traffic" "vulcan" )
+web_frameworks=( "default" "ace" "beego" "bone" "denco" "echov1" "echov2standard" "echov2fasthttp" "fasthttp-raw" \
+"fasthttprouter" "fasthttp-routing" "gas" "gin" "gocraftWeb" "goji" "gojiv2" "gojsonrest" "gorestful" "gorilla" "httprouter" \
+"httptreemux" "iris" "lars" "lion" "macaron" "martini" "pat" "possum" "r2router" "tango" "tiger" "traffic" "vulcan" )
