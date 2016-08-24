eea0324fed73b64cbb8a4d98e5bc7df8568cf1f9
smallnest <smallnest@gmail.com>
2016-08-24 16:30:17 +0800

    add latest test for Go 1.7

--- test-pipelining.sh
+++ test-pipelining.sh
@@ -67,5 +67,5 @@ echo "1000,"$(IFS=$','; echo "${test_result[*]}" ) >> concurrency-pipeline.csv
 test_all 30 5000
 echo "5000,"$(IFS=$','; echo "${test_result[*]}" ) >> concurrency-pipeline.csv
 
-mv -f processtime-pipeline.csv ./
-mv -f concurrency-pipeline.csv ./
+mv -f processtime-pipeline.csv ./testresults
+mv -f concurrency-pipeline.csv ./testresults
