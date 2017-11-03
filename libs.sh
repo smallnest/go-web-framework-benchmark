e3d03b7943dad0666363801f8964eed00f41a221
Daniele Conventi <conventi@hastega.it>
2017-11-03 17:19:22 +0100

    Cleaned for pull request.

--- libs.sh
+++ libs.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#web_frameworks=( "default" "ace" "beego" "bone" "chi" "denco" "echov3" "fasthttp-raw" \
-#"fasthttprouter" "fasthttp-routing" "fastrouter" "gear" "gin" "goji" "gojsonrest" "gongular" "gorestful" "gorilla" "go-ozzo" "gowww" "gramework" "httprouter" \
-#"httptreemux" "lars" "lion" "martini" "neo" "pat" "pure" "r2router" "siris" "tango" "tiger" "traffic" "violetear" "vulcan")
-
-web_frameworks=( "fresh" )
+web_frameworks=( "default" "ace" "beego" "bone" "chi" "denco" "echov3" "fasthttp-raw" \
+"fasthttprouter" "fasthttp-routing" "fastrouter" "gear" "gin" "goji" "gojsonrest" "gongular" "gorestful" "gorilla" "go-ozzo" "gowww" "gramework" "httprouter" \
+"httptreemux" "lars" "lion" "martini" "neo" "pat" "pure" "r2router" "siris" "tango" "tiger" "traffic" "violetear" "vulcan")
