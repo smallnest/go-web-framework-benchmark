8dbcf795e316ef99d994402c8f645d1acd1a94bc
Daniele Conventi <conventi@hastega.it>
2017-11-03 17:51:24 +0100

    Testing default template.

--- libs.sh
+++ libs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-web_frameworks=( "default" "ace" "beego" "bone" "chi" "denco" "echov3" "fasthttp-raw" \
-"fasthttprouter" "fasthttp-routing" "fastrouter" "gear" "gin" "goji" "gojsonrest" "gongular" "gorestful" "gorilla" "go-ozzo" "gowww" "gramework" "httprouter" \
-"httptreemux" "lars" "lion" "martini" "neo" "pat" "pure" "r2router" "siris" "tango" "tiger" "traffic" "violetear" "vulcan")
+#web_frameworks=( "default" "ace" "beego" "bone" "chi" "denco" "echov3" "fasthttp-raw" \
+#"fasthttprouter" "fasthttp-routing" "fastrouter" "gear" "gin" "goji" "gojsonrest" "gongular" "gorestful" "gorilla" "go-ozzo" "gowww" "gramework" "httprouter" \
+#"httptreemux" "lars" "lion" "martini" "neo" "pat" "pure" "r2router" "siris" "tango" "tiger" "traffic" "violetear" "vulcan")
+
+web_frameworks=( "beego" "echov3" "fresh" "gin" )
