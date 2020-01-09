bedd7346acad13a313b391032cccd9769394e9b0
Fenny <08jadez@gmail.com>
2020-01-09 17:02:26 +0100

    Update libs.sh

--- libs.sh
+++ libs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 web_frameworks=("default" "atreugo" "beego" "bone" "chi" "denco" "echov3" "fasthttp-raw" \
-"fasthttp-routing" "fasthttp/router" "fasthttprouter" "fastrouter" "fresh" "fiber" "gear" "gin" "goji" "gojsonrest" "gongular" "gorestful" "gorilla" "gorouter" "gorouterfasthttp" "go-ozzo" "gowww" "gramework" "httprouter" \
+"fasthttp-routing" "fasthttp/router" "fasthttprouter" "fastrouter" "fiber" "fresh" "gear" "gin" "goji" "gojsonrest" "gongular" "gorestful" "gorilla" "gorouter" "gorouterfasthttp" "go-ozzo" "gowww" "gramework" "httprouter" \
 "httptreemux" "lars" "lion" "martini" "muxie" "negroni" "neo" "pat" "pure" "r2router" "tango" "tiger" "tinyrouter" "traffic" "violetear" "vulcan" "webgo")
