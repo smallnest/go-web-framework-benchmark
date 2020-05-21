cd434773994c5f879313f60b01d80f8b0841edec
Sergio Andres Virviescas Santana <developersavsgio@gmail.com>
2020-05-21 17:38:15 +0200

    Upgrade atreugo to v11.2.1

--- server.go
+++ server.go
@@ -270,7 +270,7 @@ func atreugoHandler(ctx *atreugo.RequestCtx) error {
 }
 
 func startAtreugo() {
-	mux := atreugo.New(atreugo.Config{Addr: "127.0.0.1:" + strconv.Itoa(port)})
+	mux := atreugo.New(atreugo.Config{Addr: ":" + strconv.Itoa(port)})
 	mux.GET("/hello", atreugoHandler)
 	mux.ListenAndServe()
 }
