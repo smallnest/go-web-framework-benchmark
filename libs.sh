a47a61e950c8577115001a2f7f4e86958c12a0fb
dzw <hu.duanzhiwen@aisino.com>
2022-03-04 18:31:53 +0800

    添加 框架 aurora

--- libs.sh
+++ libs.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-web_frameworks=("aurora")
+web_frameworks=("default" "atreugo" "aurora" "beego" "bone" "chi" "clevergo" "denco" "echo" "fasthttp" \
+"fasthttp-routing" "fasthttp/router" "fasthttprouter" "fastrouter" "fiber" "flygo" "fresh" "gear" "gearbox" \
+"gin" "goframe" "goji" "gojsonrest" "gongular" "gorestful" "gorilla" "gorouter" "gorouterfasthttp" "go-ozzo" \
+"gowww" "goyave" "httprouter" "httptreemux" "lars" "lion" "martini" "muxie" "negroni" \
+"neo" "pat" "pure" "r2router" "tango" "tiger" "tinyrouter" "traffic" "treemux" "violetear" "vulcan" "webgo")
